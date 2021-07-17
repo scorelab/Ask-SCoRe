b201f1270f5e144e78b8873a9979d3cc02a9932b
Abhi Kulshrestha <40908684+AbhiKul10@users.noreply.github.com>
2021-07-17 19:22:13 +0530

    Designed Forum Screen

--- src/screens/ForumScreen/ForumScreen.js
+++ src/screens/ForumScreen/ForumScreen.js
@@ -1,18 +1,46 @@
-import React, {Component} from "react";
-import {View, SafeAreaView} from "react-native";
-import HeaderBar from "../../components/HeaderBar/HeaderBar";
+/* eslint-disable react-native/no-inline-styles */
+import React from "react";
+import {Component} from "react";
+import {View, Text, TouchableOpacity, Image, SafeAreaView} from "react-native";
 import {LOGO} from "../../config/styles.js";
+import styles from "./styles";
 
 class ForumScreen extends Component {
   render() {
     return (
       <SafeAreaView>
         <View>
-          <HeaderBar image={LOGO} title={"Forums"} />
+          <View style={{marginBottom: 15}}>
+            <View style={styles.HeaderStyle1}>
+              <Image source={LOGO} style={styles.ImageView} />
+              <Text style={styles.HeaderStyle}>Forums</Text>
+              <TouchableOpacity
+                style={styles.AskButtonStyle}
+                onPress={() => {
+                  this.props.navigation.navigate("AddChat");
+                }}>
+                <Text style={styles.TextStyle}>+</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.DividerView} />
+            <View style={styles.DividerView} />
+          </View>
+
+          <View>
+            <TouchableOpacity
+              onPress={() => this.props.navigation.push("Chat")}>
+              <View>
+                <View style={{flexDirection: "row", marginLeft: 10}}>
+                  <Image source={LOGO} style={styles.ImageView1} />
+                  <Text style={styles.TextStyle1}>GSoC</Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+            <View style={styles.DividerView1} />
+          </View>
         </View>
       </SafeAreaView>
     );
   }
 }
-
 export default ForumScreen;
---
+++ src/screens/ForumScreen/styles.js
@@ -0,0 +1,65 @@
+import {StyleSheet} from "react-native";
+
+export default StyleSheet.create({
+  HeaderStyle1: {
+    flexDirection: "row",
+  },
+
+  ImageView: {
+    width: 55,
+    height: 55,
+    marginHorizontal: 5,
+  },
+
+  ImageView1: {
+    width: 40,
+    height: 40,
+    marginHorizontal: 5,
+  },
+
+  HeaderStyle: {
+    fontSize: 30,
+    flex: 1,
+    marginLeft: 10,
+    marginTop: 5,
+  },
+
+  AskButtonStyle: {
+    width: 60,
+    height: 30,
+    backgroundColor: "#9cd683",
+    borderRadius: 10,
+    right: 10,
+    justifyContent: "center",
+    marginTop: 7,
+  },
+
+  DividerView: {
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    marginBottom: 3,
+    marginHorizontal: 10,
+  },
+
+  DividerView1: {
+    borderBottomColor: "#bfbfbf",
+    borderBottomWidth: 1,
+    marginHorizontal: 25,
+    marginTop: 2,
+    marginBottom: 5,
+  },
+
+  TextStyle: {
+    color: "black",
+    fontWeight: "500",
+    textAlign: "center",
+    fontSize: 16,
+  },
+
+  TextStyle1: {
+    alignSelf: "center",
+    fontSize: 16,
+    marginHorizontal: 15,
+    marginBottom: 2,
+  },
+});
