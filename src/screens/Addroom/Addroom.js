4400caa34b16e7b93a20799720762c26feee61fd
Abhi Kulshrestha <40908684+AbhiKul10@users.noreply.github.com>
2021-07-17 21:21:52 +0530

    Designed and Implemented Addroom Screen

--- src/screens/Addroom/Addroom.js
+++ src/screens/Addroom/Addroom.js
@@ -1,12 +1,154 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-shadow */
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable no-alert */
 import React, {Component} from "react";
-import {View, Text} from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  SafeAreaView,
+  TextInput,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
+import {firebase} from "../../config/config";
+import moment from "moment";
+import styles from "./styles.js";
+import {LOGO} from "../../config/styles.js";
 
 class Addroom extends Component {
+  state = {roomName: "", message: ""};
+
+  createRoom(roomName) {
+    const {message} = this.state;
+    var timeDate = moment();
+    firebase
+      .firestore()
+      .collection("users")
+      .doc(firebase.auth().currentUser.uid)
+      .get()
+      .then(doc => {
+        firebase
+          .database()
+          .ref(roomName)
+          .push({
+            name: doc.data().fullName,
+            message,
+            creation: new Date().toUTCString(),
+            messageTime: timeDate.format("lll"),
+            ImageURL: doc.data().downloadURL,
+          })
+          .then(data => {
+            this.setState({roomName: "", message: ""});
+            this.props.navigation.navigate("Forum");
+          })
+          .catch(error => {
+            alert(error);
+          });
+      });
+  }
   render() {
+    const {roomName, message} = this.state;
     return (
-      <View>
-        <Text>Chat Screen</Text>
-      </View>
+      <SafeAreaView style={{flex: 1}}>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+          <View>
+            <View style={{marginBottom: 15}}>
+              <View style={styles.HeaderStyle1}>
+                <Image source={LOGO} style={styles.ImageView} />
+                <Text style={styles.HeaderStyle}>Add room</Text>
+                <TouchableOpacity
+                  style={styles.AskButtonStyle}
+                  onPress={() => this.props.navigation.navigate("Forum")}>
+                  <Text style={styles.TextStyle}>BACK</Text>
+                </TouchableOpacity>
+              </View>
+
+              <View style={styles.DividerView} />
+              <View style={styles.DividerView} />
+            </View>
+            <View style={styles.ViewStyle}>
+              <Text style={styles.TextStyle1}>Enter the Room Name Here</Text>
+              {this.state.roomName.length === 0 ? (
+                <View>
+                  <View style={styles.ViewStyle1}>
+                    <TextInput
+                      style={styles.TextStyle2}
+                      title="CREATE A NEW CHAT ROOM"
+                      placeholder="ROOM NAME"
+                      value={this.state.roomName}
+                      onChangeText={roomName => this.setState({roomName})}
+                    />
+                  </View>
+                  <View style={styles.ViewStyle2}>
+                    <TextInput
+                      style={{}}
+                      title="CREATE A NEW CHAT ROOM"
+                      value={this.state.message}
+                      onChangeText={message => this.setState({message})}
+                      placeholder="Enter room description"
+                      multiline
+                    />
+                  </View>
+                  <TouchableOpacity
+                    disabled={roomName.length === 0}
+                    style={styles.TouchableStyle}
+                    onPress={() => this.createRoom(this.state.roomName)}>
+                    <Text style={{alignSelf: "center", fontSize: 20}}>
+                      CREATE
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              ) : (
+                <View>
+                  <View style={styles.ViewStyle3}>
+                    <TextInput
+                      style={styles.TextStyle2}
+                      title="CREATE A NEW CHAT ROOM"
+                      placeholder="ROOM NAME"
+                      value={this.state.roomName}
+                      onChangeText={roomName => this.setState({roomName})}
+                    />
+                  </View>
+                  {this.state.message.length === 0 ? (
+                    <View style={styles.ViewStyle2}>
+                      <TextInput
+                        style={{}}
+                        title="CREATE A NEW CHAT ROOM"
+                        placeholder="Enter room description"
+                        value={this.state.message}
+                        onChangeText={message => this.setState({message})}
+                        multiline
+                      />
+                    </View>
+                  ) : (
+                    <View style={styles.ViewStyle4}>
+                      <TextInput
+                        style={{}}
+                        title="CREATE A NEW CHAT ROOM"
+                        placeholder="Enter room description"
+                        value={this.state.message}
+                        onChangeText={message => this.setState({message})}
+                        multiline
+                      />
+                    </View>
+                  )}
+                  <TouchableOpacity
+                    disabled={roomName.length === 0}
+                    style={styles.ViewStyle5}
+                    onPress={() => this.createRoom(this.state.roomName)}>
+                    <Text style={{alignSelf: "center", fontSize: 20}}>
+                      CREATE
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              )}
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </SafeAreaView>
     );
   }
 }
---
+++ src/screens/Addroom/styles.js
@@ -0,0 +1,131 @@
+/* eslint-disable no-unused-vars */
+import {StyleSheet, Dimensions} from "react-native";
+
+let deviceWidth = Dimensions.get("window").width;
+
+export default StyleSheet.create({
+  HeaderStyle1: {
+    flexDirection: "row",
+  },
+
+  ImageView: {
+    width: 55,
+    aspectRatio: 1,
+    marginHorizontal: 5,
+  },
+
+  HeaderStyle: {
+    fontSize: 30,
+    flex: 1,
+    marginLeft: 10,
+    marginTop: 5,
+  },
+
+  AskButtonStyle: {
+    width: 60,
+    height: 30,
+    backgroundColor: "#9cd683",
+    borderRadius: 10,
+    right: 10,
+    justifyContent: "center",
+    marginTop: 7,
+  },
+
+  DividerView: {
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    marginBottom: 3,
+    marginHorizontal: 10,
+  },
+
+  DividerView1: {
+    borderBottomColor: "#51AD28",
+    borderBottomWidth: 2,
+    marginBottom: 3,
+  },
+
+  TextStyle2: {
+    flex: 1,
+    color: "black",
+    padding: 10,
+    fontSize: 20,
+  },
+
+  TextStyle: {
+    color: "black",
+    fontWeight: "500",
+    textAlign: "center",
+    fontSize: 16,
+  },
+
+  ViewStyle: {
+    justifyContent: "center",
+    alignSelf: "center",
+    marginTop: 100,
+  },
+
+  TextStyle1: {
+    fontSize: 19,
+    alignSelf: "center",
+    fontWeight: "500",
+    marginBottom: 10,
+  },
+
+  ViewStyle1: {
+    width: 250,
+    height: 60,
+    backgroundColor: "#dbdbdb",
+    justifyContent: "center",
+    borderBottomColor: "gray",
+    borderBottomWidth: 2,
+  },
+
+  ViewStyle2: {
+    width: 250,
+    minHeight: 100,
+    backgroundColor: "#dbdbdb",
+    padding: 10,
+    borderBottomColor: "gray",
+    borderBottomWidth: 2,
+    marginTop: 10,
+    borderRadius: 10,
+  },
+
+  TouchableStyle: {
+    height: 40,
+    width: 250,
+    backgroundColor: "gray",
+    justifyContent: "center",
+    marginTop: 30,
+    borderRadius: 10,
+  },
+
+  ViewStyle3: {
+    width: 250,
+    height: 60,
+    backgroundColor: "#dbdbdb",
+    justifyContent: "center",
+    borderBottomColor: "#51AD28",
+    borderBottomWidth: 2,
+  },
+
+  ViewStyle4: {
+    width: 250,
+    minHeight: 100,
+    backgroundColor: "#dbdbdb",
+    padding: 10,
+    borderBottomColor: "#51AD28",
+    borderBottomWidth: 2,
+    marginTop: 10,
+    borderRadius: 10,
+  },
+
+  ViewStyle5: {
+    height: 40,
+    width: 250,
+    backgroundColor: "#51AD28",
+    justifyContent: "center",
+    marginTop: 30,
+    borderRadius: 10,
+  },
+});
