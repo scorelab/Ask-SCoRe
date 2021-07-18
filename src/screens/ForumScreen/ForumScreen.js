1e0de577ffb987ea6bbf6ebf0bd844b212a45fe4
Abhi Kulshrestha <40908684+AbhiKul10@users.noreply.github.com>
2021-07-18 13:44:47 +0530

    Implemented Forum Screen

--- src/screens/ForumScreen/ForumScreen.js
+++ src/screens/ForumScreen/ForumScreen.js
@@ -1,12 +1,38 @@
+/* eslint-disable no-undef */
 /* eslint-disable react-native/no-inline-styles */
 import React from "react";
 import {Component} from "react";
-import {View, Text, TouchableOpacity, Image, SafeAreaView} from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  SafeAreaView,
+  FlatList,
+} from "react-native";
+import {firebase} from "../../config/config.js";
 import {LOGO} from "../../config/styles.js";
 import styles from "./styles";
 
 class ForumScreen extends Component {
+  state = {dialogvisible: false, forumtext: "", roomArray: []};
+  componentDidMount() {
+    const {roomArray} = this.state;
+    firebase
+      .database()
+      .ref()
+      .on("value", snapshot => {
+        newroomArray = [];
+        snapshot.forEach(element => {
+          newroomArray.push(element.key);
+          this.setState({roomArray: newroomArray});
+          console.log(roomArray);
+        });
+      });
+  }
+
   render() {
+    const {roomArray} = this.state;
     return (
       <SafeAreaView>
         <View>
@@ -19,28 +45,56 @@ class ForumScreen extends Component {
                 onPress={() => {
                   this.props.navigation.navigate("AddChat");
                 }}>
-                <Text style={styles.TextStyle}>+</Text>
+                <Text
+                  style={{
+                    color: "black",
+                    fontWeight: "500",
+                    textAlign: "center",
+                    fontSize: 16,
+                  }}>
+                  +
+                </Text>
               </TouchableOpacity>
             </View>
             <View style={styles.DividerView} />
             <View style={styles.DividerView} />
           </View>
-
-          <View>
-            <TouchableOpacity
-              onPress={() => this.props.navigation.push("Chat")}>
-              <View>
-                <View style={{flexDirection: "row", marginLeft: 10}}>
-                  <Image source={LOGO} style={styles.ImageView1} />
-                  <Text style={styles.TextStyle1}>GSoC</Text>
+          <FlatList
+            data={roomArray}
+            renderItem={({item}) => {
+              return (
+                <View>
+                  <TouchableOpacity
+                    onPress={() =>
+                      this.props.navigation.push("Chat", {
+                        data: item,
+                      })
+                    }>
+                    <View>
+                      <View style={{flexDirection: "row", marginLeft: 10}}>
+                        <Image source={LOGO} style={styles.ImageView1} />
+                        <Text
+                          style={{
+                            alignSelf: "center",
+                            fontSize: 16,
+                            marginHorizontal: 15,
+                            marginBottom: 2,
+                          }}>
+                          {item}
+                        </Text>
+                      </View>
+                    </View>
+                  </TouchableOpacity>
+                  <View style={styles.DividerView1} />
                 </View>
-              </View>
-            </TouchableOpacity>
-            <View style={styles.DividerView1} />
-          </View>
+              );
+            }}
+            keyExtractor={(item, index) => "key" + index}
+          />
         </View>
       </SafeAreaView>
     );
   }
 }
+
 export default ForumScreen;
