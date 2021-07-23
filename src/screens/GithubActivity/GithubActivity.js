9462c33745dc9448e32b0a604159b9bbe261b549
Abhi Kulshrestha <40908684+AbhiKul10@users.noreply.github.com>
2021-07-24 02:00:12 +0530

    Added Github and Medium Activity Screen

---
+++ src/screens/GithubActivity/GithubActivity.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import {View, Text} from "react-native";
+
+class GithubActivity extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Github Activity</Text>
+      </View>
+    );
+  }
+}
+
+export default GithubActivity;
---
+++ src/screens/MediumActivity/MediumActivity.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import {View, Text} from "react-native";
+
+class MediumActivity extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Medium Activity</Text>
+      </View>
+    );
+  }
+}
+
+export default MediumActivity;
